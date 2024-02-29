eea71cb0c5459cfcff4414bc51195f3fb9c2352b
Dura07 <Oyedejioluwasheye2013@gmail.com>
2024-02-29 07:31:41 -0800

    103-binary_tree_rotate_left

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -9,11 +9,14 @@
  */
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
-{
-	if (tree == NULL || tree->right == NUL)
-		return (tree);
 
-	binary_tree_t *new_root = tree->right;
+{
+	binary_tree_t *new_root = NULL;
+
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
+
+	new_root = tree->right;
 
 	tree->right = new_root->left;
 
